ed0346bdb593489ec389fa43916b243d7ccb0491
Anwesh43 <anweshthecool0@gmail.com>
2017-04-10 17:43:41 +0530

    Adding VideoPlayerComponent in index.js

--- index.js
+++ index.js
@@ -1,18 +1,4 @@
 import React,{Component} from 'react'
 import ReactDOM from 'react-dom'
-class Timer extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {time:0}
-    }
-    componentDidMount() {
-        const updateTime = ()=>{this.setState({time:this.state.time+1})}
-        setInterval(()=>{
-            updateTime()
-        },1000)
-    }
-    render() {
-        return <div>{this.state.time}</div>
-    }
-}
-ReactDOM.render(<Timer/>,document.getElementById('comp'))
+import VideoPlayerComponent from './components/video_player_component'
+ReactDOM.render(<VideoPlayerComponent/>,document.getElementById('comp'))
