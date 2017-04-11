3af5aacbd949e7a932fd6147f7efb27bbf8ce842
Anwesh43 <anweshthecool0@gmail.com>
2017-04-11 23:17:20 +0530

    creating a newemoji on click of a emoji button

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom'
 import VideoPlayerComponent from './components/video_player_component'
 import LikeButtonComponent from './components/LikeButtonComponent'
 class FbLikeVpComponent extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {newEmoji:undefined}
+    }
     componentDidMount() {
         const playerComp = this.refs.player
         const emojiGrpComp = this.refs.emojigrp
@@ -13,7 +17,8 @@ class FbLikeVpComponent extends Component {
             const imgButton = emojiGrpRefs[imgButtonRef]
             imgButton.onclick = ()=>{
                 likeBtn.click()
-                const emoji = imgButtonRef.replace("ref_","")
+                const newEmoji = imgButtonRef.replace("ref_","")
+                this.setState({newEmoji})
             }
         })
     }
@@ -22,7 +27,7 @@ class FbLikeVpComponent extends Component {
         const transformStr = `translateX(${w/4}px)translateY(${h/4}px)`
         const fbVideoStyle = {background:'#BDBDBD',transform:transformStr,WebkitTransform:transformStr,MozTransform:transformStr,OTransform:transformStr,width:`${w/2+w/6}px`,height:`${h/2+h/6}px`}
         return (<div style={fbVideoStyle}>
-                <VideoPlayerComponent ref="player"/>
+                <VideoPlayerComponent ref="player" newEmoji={this.state.newEmoji}/>
                 <LikeButtonComponent ref="emojigrp"/>
             </div>)
     }
