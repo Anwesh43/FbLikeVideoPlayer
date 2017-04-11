680e8db86e81493ba4fdb02e60a0309c4f900e77
Anwesh43 <anweshthecool0@gmail.com>
2017-04-11 23:16:49 +0530

    using emoji class to draw emojis in video

--- components/video_player_component.js
+++ components/video_player_component.js
@@ -1,7 +1,46 @@
 import React,{Component} from 'react'
+class Emoji {
+    constructor(x,y,w,src) {
+        this.x = x
+        this.y = y
+        this.w = w
+        this.ux = -x/40
+        this.uy = -2*w/10
+        this.limit = y-2*w
+        this.image = new Image()
+        this.image.src = `img/${src}.png`
+        this.loaded = false
+        this.image.onload = () => {
+            this.loaded = true
+        }
+      }
+      stop() {
+          return this.ux == 0 && this.uy == 0
+      }
+      render(ctx) {
+          if(this.loaded) {
+              ctx.drawImage(this.image,this.x,this.y,this.w,this.w)
+              this.x+=this.ux
+              this.y+=this.uy
+              if(this.y<=this.limit) {
+                  this.uy = Math.abs(this.uy)
+              }
+              if(this.y>=this.limit+2*this.w) {
+                  this.uy = -Math.abs(this.uy)
+              }
+              if(this.x<0) {
+                  this.ux = 0
+                  this.uy = 0
+              }
+          }
+
+      }
+  }
+
 export default class VideoPlayerComponent extends Component {
     constructor(props) {
         super(props)
+        this.emojis = []
     }
     componentDidMount() {
         const canvas = this.refs.c1
@@ -19,10 +58,20 @@ export default class VideoPlayerComponent extends Component {
         setInterval(()=>{
             context.clearRect(0,0,w,h)
             context.drawImage(video,0,0,w,h)
+            this.emojis.forEach((emoji,index)=>{
+                emoji.render(context)
+                console.log(emoji.x)
+                if(emoji.stop()) {
+                    this.emojis.splice(index,1)
+                }
+            })
         },100)
     }
     render() {
         const w = window.innerWidth,h = window.innerHeight
+        if(this.props.newEmoji!=undefined) {
+            this.emojis.push(new Emoji(w/2,6*h/20,w/40,this.props.newEmoji))
+        }
 
         const transformStr = `translateX(${w/12}px)translateY(${-h/2+h/12}px)`
         const videoStyle={visibility:'hidden'}
